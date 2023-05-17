2c1b2057c00e719f92bcd8159096ec34b79ed220
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-17 18:39:51 +0700

    membuat class mahasiswa

---
+++ constructor/constructor.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa();
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+mahasiswa::mahasiswa() {
+	nim = 0;
+	nama = "";
+}
+
+mahasiswa::mahasiswa(int iNim) {
+	nim = iNim; 
+}
+
+mahasiswa::mahasiswa(string iNama) {
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void mahasiswa::cetak() {
+	cout << endl << "Nim = " << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
