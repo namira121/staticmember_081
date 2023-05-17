22d84a7aa4773b3aec28479bd02b4c44f98448db
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-17 19:01:17 +0700

    memasukkan member member ke dalam class angka termasuk destructor dengan syntax ~

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -1,20 +1,46 @@
-// destructor.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class angka {
+private:
+	int* arr;
+	int panjang;
+public:
+	angka(int); //constructor 
+	void cetakData(); 
+	void isiData();
+};
+//Definisi member function
+angka::angka(int i) { //constructor
+	panjang = i;
+	arr = new int[i];
+	isiData();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+angka::~angka() { //Desctuctor
+	cout << endl;
+	cetakData();
+	delete[]arr;
+	cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void angka::cetakData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isiData() {
+	for (int i = 1;i <= panjang;i++) {
+		cout << i << " = "; cin >> arr[i]; 
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //Constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); //Constructor Dipanggil
+	delete ptrBelajarcpp; //Destructor Dipanggil
+
+	return 0;
+} //Destructor Dipanggil
