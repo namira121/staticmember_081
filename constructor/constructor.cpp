a7252a8880431e4242a381913d99e942cb21f6a5
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-17 18:40:42 +0700

    menambahkan member pada class mahasiswa

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 class mahasiswa {
 private:
-	int nim;
-	string nama;
+	int nim;  
+	string nama;  
 public:
 	mahasiswa();
 	mahasiswa(int);
@@ -34,4 +34,18 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 void mahasiswa::cetak() {
 	cout << endl << "Nim = " << nim << endl;
 	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30,"Fauzan"); 
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
 }
