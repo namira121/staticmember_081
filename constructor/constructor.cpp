f209681d786982d1d4466a508f2bc3083e622e80
namira121 <127211008+namira121@users.noreply.github.com>
2023-05-17 18:47:28 +0700

    prosedur untuk menampilkan nim dan nama

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -33,7 +33,7 @@ mahasiswa::mahasiswa(int iNim, string iNama) {
 }
 void mahasiswa::cetak() {
 	cout << endl << "Nim = " << nim << endl;
-	cout << "Nama = " << nama << endl;
+	cout << "Nama = " << nama << endl; 
 }
 
 int main() {
